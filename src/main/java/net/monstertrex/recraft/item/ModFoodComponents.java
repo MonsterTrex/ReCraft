5a5bbdbe66a7bd85b6324656ced2fd36417fb227
MonsterTrex <AdrianGabrysz0508@gmail.com>
2022-05-01 02:01:20 +0100

    Balancing and Bug Fix

--- src/main/java/net/monstertrex/recraft/item/ModFoodComponents.java
+++ src/main/java/net/monstertrex/recraft/item/ModFoodComponents.java
@@ -11,7 +11,7 @@ public class ModFoodComponents {
             .build();
 
     public static FoodComponent GOLDEN_BEETROOT = new FoodComponent.Builder()
-            .hunger(4).saturationModifier(0.4f)
+            .hunger(4).saturationModifier(1.0f)
             .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.FIRE_RESISTANCE, 400), 1f)
             .build();
 
@@ -21,7 +21,7 @@ public class ModFoodComponents {
             .build();
 
     public static FoodComponent GOLDEN_BREAD = new FoodComponent.Builder()
-            .hunger(10).saturationModifier(1.0f)
+            .hunger(8).saturationModifier(1.2f)
             .build();
 
     public static FoodComponent GOLDEN_CHORUS_FRUIT = new FoodComponent.Builder()
@@ -30,42 +30,42 @@ public class ModFoodComponents {
             .build();
 
     public static FoodComponent GOLDEN_COOKED_BEEF = new FoodComponent.Builder()
-            .hunger(10).saturationModifier(1.4f)
+            .hunger(10).saturationModifier(0.8f).meat()
             .build();
 
     public static FoodComponent GOLDEN_COOKED_CHICKEN = new FoodComponent.Builder()
-            .hunger(8).saturationModifier(0.8f)
+            .hunger(8).saturationModifier(0.6f).meat()
             .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.SLOW_FALLING, 800), 1f)
             .build();
 
     public static FoodComponent GOLDEN_COOKED_COD = new FoodComponent.Builder()
-            .hunger(6).saturationModifier(0.8f)
+            .hunger(6).saturationModifier(0.6f)
             .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.DOLPHINS_GRACE, 600), 1f)
             .statusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 600), 1f)
             .build();
 
     public static FoodComponent GOLDEN_COOKED_MUTTON = new FoodComponent.Builder()
-            .hunger(8).saturationModifier(1.4f)
+            .hunger(8).saturationModifier(0.8f).meat()
             .build();
 
     public static FoodComponent GOLDEN_COOKED_PORKCHOP = new FoodComponent.Builder()
-            .hunger(10).saturationModifier(1.4f)
+            .hunger(10).saturationModifier(0.8f).meat()
             .build();
 
     public static FoodComponent GOLDEN_COOKED_RABBIT = new FoodComponent.Builder()
-            .hunger(7).saturationModifier(0.8f)
+            .hunger(7).saturationModifier(0.6f).meat()
             .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 900), 1f)
             .build();
 
     public static FoodComponent GOLDEN_COOKED_SALMON = new FoodComponent.Builder()
-            .hunger(7).saturationModifier(1.0f)
+            .hunger(7).saturationModifier(0.8f)
             .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.DOLPHINS_GRACE, 900), 1f)
             .statusEffect(new StatusEffectInstance(StatusEffects.WATER_BREATHING, 900), 1f)
             .build();
 
     public static FoodComponent GOLDEN_GLOW_BERRIES = new FoodComponent.Builder()
             .hunger(4).saturationModifier(0.4f)
-            .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 300), 1f)
+            .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 600), 1f)
             .build();
 
     public static FoodComponent GOLDEN_PUMPKIN_PIE = new FoodComponent.Builder()
@@ -75,7 +75,7 @@ public class ModFoodComponents {
 
     public static FoodComponent GOLDEN_SWEET_BERRIES = new FoodComponent.Builder()
             .hunger(4).saturationModifier(0.4f)
-            .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 600), 1f)
+            .alwaysEdible().statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 900), 1f)
             .build();
 
 }
